500c5ec19152f8e66c1d0d3b3c162fde5c845784
Brett Bilyk <brettbilyk@gmail.com>
2022-01-05 15:44:16 -0700

    Finish assignment to learn active record

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts total_revenue = Store.sum(:annual_revenue)
+puts total_revenue / Store.count
+
+puts num_stores_over_1M = Store.where(["annual_revenue < :annual_revenue", {annual_revenue: 1000000}]).count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employee.create(first_name: "Brett", last_name: "Bilyk", hourly_rate: 60)
+@store2.employee.create(first_name: "Samantha", last_name: "Odegard", hourly_rate: 70)
+@store4.employee.create(first_name: "Mathew", last_name: "Bilyk", hourly_rate: 41)
